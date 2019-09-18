2bf40dcd4e2f067d63e666609d43c8cc02e81cf0
rutujaBacchuwar <rutuja.bacchuwar@stackroute.in>
2019-09-18 14:56:57 +0530

    final code

---
+++ src/main/java/com/stackroute/chat/controller/ChatController.java
@@ -0,0 +1,29 @@
+package com.stackroute.chat.controller;
+
+import com.stackroute.chat.model.Chat;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Chat sendMessage(@Payload Chat chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Chat addUser(@Payload Chat chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
